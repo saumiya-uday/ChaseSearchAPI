73ae944ca497f3b83de2d42d9e26d814ec519cda
saumiya-uday <saumiya@SomsMacBookPro.local>
2016-12-04 17:55:42 +0530

    Initial commit

---
+++ ChaseSearchAPI/ChaseSearchAPI.h
@@ -0,0 +1,19 @@
+//
+//  ChaseSearchAPI.h
+//  ChaseSearchAPI
+//
+//  Created by Saumiya on 04/12/16.
+//  Copyright © 2016 Saumiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChaseSearchAPI.
+FOUNDATION_EXPORT double ChaseSearchAPIVersionNumber;
+
+//! Project version string for ChaseSearchAPI.
+FOUNDATION_EXPORT const unsigned char ChaseSearchAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChaseSearchAPI/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-ChaseSearchAPI/Pods-ChaseSearchAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChaseSearchAPI : NSObject
+@end
+@implementation PodsDummy_Pods_ChaseSearchAPI
+@end
